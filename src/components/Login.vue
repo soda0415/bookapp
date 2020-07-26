16df1e8c066d9e5c99448a8bbdebcf421b5fbd10
soda <atsushi037088@gmail.com>
2020-07-26 22:51:32 +0900

    commit

---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <section class="hero is-light is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="column is-4 is-offset-4">
+            <div class="box">
+               <div class="field">
+                 <div class="control">
+                   <input class="input is-large" type="email" placeholder="Eメール" v-model="email" autofocus="" v-validate="'required|email'" name="email">
+                 </div>
+              </div>
+              <div class="field">
+                <div class="control">
+                  <input class="input is-large" type="password" placeholder="パスワード" v-model="password" v-validate="'required|min:6|max:20'" maxlength="20" name="password">
+                </div>
+              </div>
+              <div class="field">
+                <label class="checkbox">
+                <input type="checkbox">
+                ログインしたままにする
+                </label>
+               </div>
+               <button class="button is-block is-info is-large is-fullwidth" @click="login()" :disabled="!isValidated" >ログイン</button>
+              </div>
+              <p class="has-text-grey">
+                <a href="..">パスワードを忘れた方はこちら</a>
+              </p>
+            </div>
+          </div>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import 'bulma/css/bulma.css'; 
+export default {
+  name: 'Login',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="loginpage">
+    <Login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'LoginPage',
+  components: {
+    Login
+  }
+}
+</script>
